459d469274d03bfbc85bb2a9e64a75f405829a17
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2015-08-20 16:27:11 +0100

    do not fail analysis if pattern from config is not in spec

--- src/main/scala/codacy/dockerApi/DockerEnvironment.scala
+++ src/main/scala/codacy/dockerApi/DockerEnvironment.scala
@@ -9,14 +9,15 @@ import scala.util.{Failure, Success, Try}
 
 trait DockerEnvironment {
 
-  def config(implicit spec: Spec): Try[Option[FullConfig]] = Try(Files.readAllBytes(configFilePath)).transform(
-    raw => Try(Json.parse(raw)).flatMap(
-      _.validate[FullConfig].fold(
-        asFailure,
-        conf => Success(Option(conf))
-      )),
-    _ => Success(Option.empty[FullConfig])
-  )
+  def config(implicit spec: Spec): Try[Option[FullConfig]] =
+    Try(Files.readAllBytes(configFilePath)).transform(
+      raw => Try(Json.parse(raw)).flatMap(
+        _.validate[FullConfig].fold(
+          asFailure,
+          conf => Success(Option(conf))
+        )),
+      _ => Success(Option.empty[FullConfig])
+    )
 
   lazy val spec: Try[Spec] = {
     Try(
